900ada829dc5fd3e8a13f61035102d5d337c7753
BlueRose0528 <jrhool@naver.com>
2025-07-10 21:42:23 +0900

    Import seeso project as normal folders (remove nested .git)

---
+++ seeso/seeso-sample-web/devServer.js
@@ -0,0 +1,33 @@
+const Bundler = require('parcel-bundler');
+const express = require('express');
+const http = require('http');
+const open = require('open');
+const app = express();
+
+const bundlePath = process.argv[2];
+const port = process.argv[3];
+
+app.use((req, res, next) => {
+    res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    next()
+})
+
+const bundler = new Bundler(bundlePath);
+
+app.use(bundler.middleware());
+// data 폴더 정적 제공 설정 추가
+app.use('/data', express.static(__dirname + '/data'));
+
+
+const server = http.createServer(app);
+server.listen(port);
+
+
+server.on('error', (err) => console.error(err));
+server.on('listening', () => {
+    console.info('Server is running');
+    console.info(`  NODE_ENV=[${process.env.NODE_ENV}]`);
+    console.info(`  Port=[${port}]`);
+    open(`http://localhost:${port}`);
+});
